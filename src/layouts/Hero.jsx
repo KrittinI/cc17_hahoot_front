71c6ff5f0e7131d5cb6911651e3bc59b5830aa56
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-06-23 16:46:19 +0700

    update-bug

--- src/layouts/Hero.jsx
+++ src/layouts/Hero.jsx
@@ -12,13 +12,13 @@ export default function HeroContianer() {
     <div className="mt-20 mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-10 px-4 py-10 sm:px-6 sm:py-10 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
       {/* **************************** left container ************************* */}
       <div className="bg-white p-10 h-full rounded-lg">
-        {heroCard.map((heroCard) => (
-          <>
+        {heroCard.map((heroCard, index) => (
+          <div key={index}>
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl">
               {heroCard.heroTitle}
             </h2>
             <p className="mt-4 text-gray-500">{heroCard.heroDescription}</p>
-          </>
+          </div>
         ))}
         {/* button see all */}
         {/* <div className="flex w-full items-center justify-start rounded-md border border-transparent py-3 text-base font-medium">
@@ -31,14 +31,14 @@ export default function HeroContianer() {
 
       {/* **************************** right container ************************* */}
       <div className="bg-white p-4 h-full rounded-lg">
-        {heroCard.map((heroCard) => (
-          <>
+        {heroCard.map((heroCard,index) => (
+          <div key={index}>
             <img
               src={heroCard.heroImage}
               alt="hero-image"
               className="h-[240px] w-[800px]"
             />
-          </>
+          </div>
         ))}
       </div>
       {/* **************************** right container ************************* */}
