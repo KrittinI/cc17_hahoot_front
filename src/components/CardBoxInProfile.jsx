81aa2e2b4faedb45875cd3172656a1f792e325d7
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-06-27 13:14:50 +0700

    ui-userProfile

--- src/components/CardBoxInProfile.jsx
+++ src/components/CardBoxInProfile.jsx
@@ -1,18 +1,24 @@
-
 import DeleteIcon from "../icons/delete";
 import EditIcon from "../icons/edit";
 import PreviewIcon from "../icons/preview";
 
-export default function CardBoxInProfile({ image, width = 20, onClick,title }) {
+export default function CardBoxInProfile({
+  image,
+  width = 20,
+  onClick,
+  title,
+}) {
   return (
     <div
       className={`bg-white flex flex-col justify-center shadow-xl rounded-lg w-[${width}%]`}
     >
-      <div className="bg-purple-400 h-[50%] rounded-t-lg">
-         <img className="rounded-t-lg" src={image} /> 
+      <div className="h-[50%] rounded-t-lg">
+        <img className="rounded-t-lg" src={image} />
       </div>
       <div className="grid grid-cols-1 gap-3 p-3 rounded-lg">
-        <div className="{`text-${size}`} text-font-title-card h-10">{title}</div>
+        <div className="{`text-${size}`} text-font-title-card h-10">
+          {title}
+        </div>
         <div className="flex justify-around">
           <div
             role="button"
