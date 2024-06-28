0b9365fc444b290c66a13caedbf7dfeb1466dae3
playvolley <ratchanon_cu@outlook.com>
2024-06-28 12:22:55 +0700

    AddCommentV2

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import Ranking from "./Ranking";
-//AddComment
+//AddCommentXX
 const socket = io("http://localhost:4000");
 
 const Quiz = () => {
