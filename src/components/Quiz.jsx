7074f22ed6db7488f524ff03e04cf6022ab01ac7
playvolley <ratchanon_cu@outlook.com>
2024-07-03 00:56:22 +0700

    BG

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -108,64 +108,72 @@ export default function Quiz() {
   const { question, options, answer, image } = quizData[currentQuestionIndex];
 
   return (
-    <div className="flex flex-col items-center justify-center h-[calc(100vh-12rem)] w-[75%] gap-12 transition-all duration-500 ease-in-out transform">
-      <div className="bg-white shadow-lg rounded-lg p-12 w-full transition-transform duration-500 ease-in-out transform hover:scale-105">
-        <h2 className="text-font-title text-center animate-none">{question}</h2>
-      </div>
-      <div className="flex flex-row justify-between items-center my-4 gap-60 animate-fade-in">
-        <span
-          className="flex items-center justify-center text-6xl text-white font-bold bg-timeLeft shadow-lg rounded-full w-32 h-32"
-          style={{ minWidth: "40px" }}
-        >
-          {timeLeft}
-        </span>
-        <img
-          className="w-[420px] h-[250px] rounded-lg transition-transform duration-500 ease-in-out transform hover:scale-105"
-          src={image}
-          alt="Quiz Image"
-        />
-        <button
-          className={`rounded-lg w-32 h-12 shadow-lg text-lg font-bold ${
-            selectedAnswer || showAnswer
-              ? "bg-white text-black animate-bounce"
-              : "bg-grey text-white invisible"
-          } transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl`}
-          onClick={handleShowScoreboard}
-          disabled={!selectedAnswer && !showAnswer}
-        >
-          Next
-        </button>
-      </div>
-      <div className="grid grid-cols-2 gap-2 w-full">
-        {options.map((option, index) => (
-          <button
-            key={option}
-            onClick={() => handleAnswerClick(option)}
-            className={`px-10 py-10 text-white text-font-title text-start animate-pop ${
-              showAnswer
-                ? option === quizData[currentQuestionIndex].answer
-                  ? "bg-darkgreen"
-                  : selectedAnswer === option
-                  ? "bg-darkred"
-                  : "bg-red opacity-80"
-                : `${buttonColors[index]} ${hoverColors[index]}`
-            } flex justify-between items-center transition-all duration-500 ease-in-out transform hover:scale-105`}
-            disabled={showAnswer || selectedAnswer}
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center h-[calc(100vh-12rem)] w-[75%] gap-12 transition-all duration-300 ease-in-out transform">
+        <div className="bg-white shadow-lg rounded-lg p-12 w-full transition-transform duration-500 ease-in-out transform hover:scale-105">
+          <h2 className="text-font-title text-center animate-none">
+            {question}
+          </h2>
+        </div>
+        <div className="flex flex-row justify-between items-center my-4 gap-60 animate-fade-in">
+          <span
+            className={`flex items-center justify-center text-6xl text-white font-bold bg-timeLeft shadow-lg rounded-full w-32 h-32 ${
+              selectedAnswer || showAnswer ? "invisible" : ""
+            }`}
+            style={{ minWidth: "40px" }}
           >
-            <div className="flex items-center">
-              {icons[index]}&nbsp;&nbsp;{option}
-            </div>
-            {showAnswer && (
-              <div className="ml-2">
-                {option === quizData[currentQuestionIndex].answer ? (
-                  <CheckTrue />
-                ) : (
-                  <CheckFalse />
-                )}
-              </div>
-            )}
+            {timeLeft}
+          </span>
+          <img
+            className={`w-[420px] h-[250px] rounded-lg ${
+              selectedAnswer || showAnswer ? "invisible" : ""
+            }`}
+            src={image}
+            alt="Quiz Image"
+          />
+          <button
+            className={`rounded-lg w-32 h-12 shadow-lg text-lg font-bold ${
+              selectedAnswer || showAnswer
+                ? "bg-white text-black animate-bounce"
+                : "bg-grey text-white invisible"
+            } transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl`}
+            onClick={handleShowScoreboard}
+            disabled={!selectedAnswer && !showAnswer}
+          >
+            Next
           </button>
-        ))}
+        </div>
+        <div className="grid grid-cols-2 gap-2 w-full">
+          {options.map((option, index) => (
+            <button
+              key={option}
+              onClick={() => handleAnswerClick(option)}
+              className={`px-10 py-10 text-white text-font-title text-start animate-pop ${
+                showAnswer
+                  ? option === quizData[currentQuestionIndex].answer
+                    ? "bg-darkgreen"
+                    : selectedAnswer === option
+                    ? "bg-darkred"
+                    : "bg-red opacity-80"
+                  : `${buttonColors[index]} ${hoverColors[index]}`
+              } flex justify-between items-center transition-all duration-500 ease-in-out transform hover:scale-105`}
+              disabled={showAnswer || selectedAnswer}
+            >
+              <div className="flex items-center">
+                {icons[index]}&nbsp;&nbsp;{option}
+              </div>
+              {showAnswer && (
+                <div className="ml-2">
+                  {option === quizData[currentQuestionIndex].answer ? (
+                    <CheckTrue />
+                  ) : (
+                    <CheckFalse />
+                  )}
+                </div>
+              )}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -3,8 +3,8 @@ import Quiz from "../components/Quiz";
 
 export default function QuizPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <>
       <Quiz />
-    </div>
+    </>
   );
 }
