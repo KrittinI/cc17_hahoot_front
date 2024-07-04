0bafc17dfccbaf6c8569952e5ecee8ef49784aa8
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-07-04 15:09:21 +0700

    modify-footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,8 +2,8 @@ import Logo from "../icons/Logo";
 
 export default function Footer() {
   return (
-    <div className="bg-white h-[136px] flex flex-col justify-center items-center ">
-      <div className="text-sm mb-5">
+    <div className="bg-white h-[100px] flex flex-col justify-center items-center gap-3 ">
+      <div className="text-sm">
         Copyright © 2025 BRIX Agency | All Rights Reserved
       </div>
       <Logo />
