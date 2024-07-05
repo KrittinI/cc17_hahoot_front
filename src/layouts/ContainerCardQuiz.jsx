948182f97fdb936f5b504945035536eb05017965
KrittinI <krittin.ikk@gmail.com>
2024-07-05 10:34:37 +0700

    edit padding

--- src/layouts/ContainerCardQuiz.jsx
+++ src/layouts/ContainerCardQuiz.jsx
@@ -5,7 +5,7 @@ export default function ContainerCardQuiz({ hero }) {
   return (
     <div className="">
       <div className="mx-auto max-w-2xl px-4 py-5 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className=" grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           <CardQuizHome
             data={hero?.question1}
             bg={(1 + randomNum) % 4}
