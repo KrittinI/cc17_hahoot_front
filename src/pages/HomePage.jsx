52545e3f2a8392c14c760f55ae9695b70997a0cf
KrittinI <krittin.ikk@gmail.com>
2024-07-05 10:05:59 +0700

    homePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,12 +6,20 @@ import Modal from "../components/Modal";
 import { useState } from "react";
 // import PlayGameBox from "../components/PlayGameBox";
 import EditProfileForm from "../features/userProfile/components/EditProfileForm";
+import axios from "../config/axios";
+
 
 export default function HomePage() {
   const { authUser } = useAuth()
   const [open, setOpen] = useState(false)
-
+  const [hero, setHero] = useState(null)
+  console.log(hero);
   useEffect(() => {
+    const fetchHero = async () => {
+      const res = await axios.get('/hero/Active')
+      setHero(res.data.hero)
+    }
+    fetchHero()
     if (authUser?.username === null) {
       setOpen(true)
     }
@@ -20,9 +28,9 @@ export default function HomePage() {
 
   return (
     <>
-      <div className="h-[calc(100vh-8rem)] ">
-        <HeroContianer />
-        <ContianerCardQuiz />
+      <div className="">
+        <HeroContianer hero={hero} />
+        <ContianerCardQuiz hero={hero} />
         <Modal open={open} title={'Choose Your Avatar and Username'}>
           <EditProfileForm onSuccess={() => setOpen(false)} />
         </Modal>
