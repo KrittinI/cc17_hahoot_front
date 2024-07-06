7faa8bbc3da2b802347760e2ba0c42e1e40fbb5f
KrittinI <krittin.ikk@gmail.com>
2024-07-06 18:51:53 +0700

    clear log

--- src/features/myQuiz-Event/components/MyFavoriteQuizEvent.jsx
+++ src/features/myQuiz-Event/components/MyFavoriteQuizEvent.jsx
@@ -2,7 +2,6 @@ import CardBoxInProfile from "../../../components/CardBoxInProfile";
 import Cardcontainer from "../../../components/CardContainer";
 
 export default function MyFavoriteQuizEvent({ title, showCard }) {
-  console.log("showCard", showCard);
   return (
     <div className="h-full">
       <Cardcontainer title={title}>
