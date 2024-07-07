f6dea584129da05adaf6744e7625bf38c8c218a1
KrittinI <krittin.ikk@gmail.com>
2024-07-07 15:14:22 +0700

    edit image in card

--- src/components/CardBoxInProfile.jsx
+++ src/components/CardBoxInProfile.jsx
@@ -12,19 +12,20 @@ export default function CardBoxInProfile({ data, name, type }) {
   const navigate = useNavigate();
   return (
     <div
-      role="button"
       // className={`${sizeMap[width]} bg-white flex flex-col shadow-xl rounded-lg`}
-      className="relative text-left inline-block cursor-pointer w-[190px] h-[240px] rounded-[8px] shadow-xl z-80"
+      className="relative text-left inline-block cursor-pointer w-[190px] h-[240px] rounded-[8px] shadow-md z-80"
       onClick={() => navigate(`/${name}/${data?.id}`)}
     >
       <div className="bg-white w-[30px] h-[30px] rounded-full absolute right-2 top-2 flex justify-center items-center text-[#D7D7D7]">
         {data?.QuestionFavorite?.length || data?.EventFavorites?.length ? <HeartIcon /> : <HeartIconUnfav />}
       </div>
-      <img
-        className="h-3/6 w-full rounded-t-[8px]"
-        src={data?.questionPicture || data?.eventImage || image}
-        alt={name}
-      />
+      <div className="bg-gray-100 rounded-t-[8px] flex justify-center">
+        <img
+          className="max-w-full min-h-[100px] max-h-[100px]"
+          src={data?.questionPicture || data?.eventImage || image}
+          alt={name}
+        />
+      </div>
       <div className="grid grid-cols-1 gap-3 p-2 rounded-lg ">
         <div className="text-font-title-card h-10 overflow-hidden text-ellipsis whitespace-nowrap">
           {data?.question || data?.eventName}
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import Logo from "../icons/Logo";
 
 export default function Footer() {
   return (
-    <div className="bg-white h-[100px] flex flex-col justify-center items-center gap-3 ">
+    <div className=" bg-white h-[100px] flex flex-col justify-center items-center gap-3 z-40">
       <div className="text-sm">
         Copyright © 2025 BRIX Agency | All Rights Reserved
       </div>
