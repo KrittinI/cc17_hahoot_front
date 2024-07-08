343122d3480f143d63822d151b552f5dbd4b3b5c
KrittinI <krittin.ikk@gmail.com>
2024-07-08 14:19:18 +0700

    quesiton Context

--- src/contexts/QuestionContext.jsx
+++ src/contexts/QuestionContext.jsx
@@ -9,13 +9,8 @@ export const QuestionContext = createContext();
 export default function QuestionContextProvider({ children }) {
   const { authUser } = useAuth();
   const [question, setQuestion] = useState([]);
-
   const [showQuestion, setShowQuestion] = useState([]);
-  const [playQuestion, setPlayQuestion] = useState([]);
-
-  const [quizTopic, setQuizTopic] = useState([]);
-  const [seeAll, setSeeAll] = useState(true);
-  const [search, setSearch] = useState("");
+  const [playQuestion, setPlayQuestion] = useState([])
 
   const getAllQuestion = async () => {
     const res = await questionApi.getAllQuestion();
@@ -23,54 +18,20 @@ export default function QuestionContextProvider({ children }) {
     setShowQuestion(res.data.questions);
   };
 
-  const getQuestionByUserId = async (id) => await questionApi.getQuestionByUserId(id);
+  const getQuestionByUserId = async (id) =>
+    await questionApi.getQuestionByUserId(id);
 
   const getQuestionByQuestionId = async (id) => {
     const res = await questionApi.getQuestionByQuestionId(id);
     return res.data.question;
   };
 
-  const getQuestionByTopicId = async (topicId) => {
-    const res = await questionApi.getQuestionByTopicId(topicId);
-    setQuizTopic(res.data.questions);
-  };
-
-  const getFavQuestion = async (id) => {
-    const res = await questionApi.getFavQuestion(id);
-    return res.data.questions;
-  };
-
-  const createQuestion = async (body) => {
-    await questionApi.createQuestion(body);
-  };
-
-  const editQuestion = async (id, body) => {
-    await questionApi.editQuestionById(id, body);
-  };
-
-  const isSeeAll = () => {
-    if (seeAll) {
-      if (search) {
-        setShowQuestion(question?.filter((el) => el.question.toLowerCase().includes(search)));
-      } else {
-        setShowQuestion(question);
-      }
-    } else {
-      if (search) {
-        setShowQuestion(quizTopic?.filter((el) => el.question.toLowerCase().includes(search)));
-      } else {
-        setShowQuestion(quizTopic);
-      }
-    }
-  };
+  const getQuestionByTopicId = async (topicId) =>
+    await questionApi.getQuestionByTopicId(topicId);
 
   useEffect(() => {
     getAllQuestion();
   }, [authUser]);
-
-  useEffect(() => {
-    isSeeAll();
-  }, [seeAll, question, quizTopic, search]);
 
   const value = {
     question,
@@ -81,15 +42,12 @@ export default function QuestionContextProvider({ children }) {
     getQuestionByQuestionId,
     setPlayQuestion,
     playQuestion,
-    getFavQuestion,
-    createQuestion,
-    editQuestion,
-    isSeeAll,
-    quizTopic,
-    setSearch,
-    setSeeAll,
     setQuestion
   };
 
-  return <QuestionContext.Provider value={value}>{children}</QuestionContext.Provider>;
+  return (
+    <QuestionContext.Provider value={value}>
+      {children}
+    </QuestionContext.Provider>
+  );
 }
