d070ff43f0ba2fb3ec2139b016a6a9956bebcd3c
playvolley <ratchanon_cu@outlook.com>
2024-07-09 10:44:40 +0700

    FixedAnsCount

--- src/features/multiplayer/components/MultiPlayer.jsx
+++ src/features/multiplayer/components/MultiPlayer.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react-hooks/exhaustive-deps */
+
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 import Loading from "../../../components/Loading";
-// import ClientAnswerResult from "./ClientAnswerResult";
 import ScoreboardMultiplayer from "./ScoreboardMultiplayer";
 import useAuth from "../../../hooks/useAuth";
 import useQuestion from "../../../hooks/useQuestion";
@@ -15,9 +15,9 @@ import ShowQuestion from "./ShowQuestion";
 import useEvent from "../../../hooks/useEvent";
 
 const MultiPlayer = () => {
-  const { authUser } = useAuth()
-  const { playQuestion } = useQuestion()
-  const { eventId } = useEvent()
+  const { authUser } = useAuth();
+  const { playQuestion } = useQuestion();
+  const { eventId } = useEvent();
   const [roomId, setRoomId] = useState("");
   const [isOwner, setIsOwner] = useState(false);
   const [isStarted, setIsStarted] = useState(false);
@@ -26,7 +26,6 @@ const MultiPlayer = () => {
   const [showAnswer, setShowAnswer] = useState(false);
   const [score, setScore] = useState(0);
   const [timeLeft, setTimeLeft] = useState(null);
-  const [totalTimeLeft, setTotalTimeLeft] = useState(0);
   const [hasJoined, setHasJoined] = useState(false);
   const [loading, setLoading] = useState(false);
   const [clientAnswerResult, setClientAnswerResult] = useState(null);
@@ -54,7 +53,11 @@ const MultiPlayer = () => {
     // const newSocket = io('http://localhost:4000'); // หรือ URL ของเซิร์ฟเวอร์จริง
     setSocket(socketIo);
     if (authUser && playQuestion.length) {
-      socketIo.emit("createRoom", { name: authUser?.username, questions: playQuestion, eventId })
+      socketIo.emit("createRoom", {
+        name: authUser?.username,
+        questions: playQuestion,
+        eventId,
+      });
     }
     // count room answer
     socketIo.on("RoomAnswerCount", (counts) => {
@@ -166,7 +169,7 @@ const MultiPlayer = () => {
       socketIo.off("answerCount");
       socketIo.off("RoomAnswerCount");
       // เอามาไว้ disconnect ออก หากกด ออก
-      socketIo.disconnect()
+      socketIo.disconnect();
     };
   }, []);
 
@@ -190,7 +193,6 @@ const MultiPlayer = () => {
     setShowAnswer(false);
     setScore(0);
     setTimeLeft(null);
-    setTotalTimeLeft(0);
     setHasJoined(false);
     setLoading(false);
     setClientAnswerResult(null);
@@ -228,38 +230,51 @@ const MultiPlayer = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="flex flex-col items-center justify-center h-[calc(100vh-12rem)] w-full gap-12 transition-all duration-300 ease-in-out transform">
-        {
-          loading ? (
-            <Loading />
-          ) : !hasJoined ? (
-            // Form to Join Game Room
-            <JoinRoomForm roomId={roomId} setRoomId={setRoomId} socket={socket} />
-          ) : !isStarted ? (
-            // Waiting Room to Start
-            <WaitingRoom socket={socket} players={players} roomId={roomId} isOwner={isOwner} />
-          ) : isOwner && showScoreboard ? (
-            <ScoreboardMultiplayer
-              players={playerInfo}
-              socket={socket}
-              newRoomId={newRoomId}
-              isGameOver={isGameOver}
-            />
-          ) : isOwner && currentQuestion ? (
-            <ShowQuestion currentQuestion={currentQuestion} showAnswer={showAnswer} roomAnswerCount={roomAnswerCount} onClick={handleShowScoreboard} timeLeft={timeLeft} answerCount={answerCount} />
-          ) : clientAnswerResult !== null ? (
-            <ShowResultBox clientAnswerResult={clientAnswerResult} />
-          ) : !isOwner && currentQuestion ? (
-            // role === Player
-            <PlayerChoice choice={[
+        {loading ? (
+          <Loading />
+        ) : !hasJoined ? (
+          // Form to Join Game Room
+          <JoinRoomForm roomId={roomId} setRoomId={setRoomId} socket={socket} />
+        ) : !isStarted ? (
+          // Waiting Room to Start
+          <WaitingRoom
+            socket={socket}
+            players={players}
+            roomId={roomId}
+            isOwner={isOwner}
+          />
+        ) : isOwner && showScoreboard ? (
+          <ScoreboardMultiplayer
+            players={playerInfo}
+            socket={socket}
+            newRoomId={newRoomId}
+            isGameOver={isGameOver}
+          />
+        ) : isOwner && currentQuestion ? (
+          <ShowQuestion
+            currentQuestion={currentQuestion}
+            showAnswer={showAnswer}
+            roomAnswerCount={roomAnswerCount}
+            onClick={handleShowScoreboard}
+            timeLeft={timeLeft}
+            answerCount={answerCount}
+          />
+        ) : clientAnswerResult !== null ? (
+          <ShowResultBox clientAnswerResult={clientAnswerResult} />
+        ) : !isOwner && currentQuestion ? (
+          // role === Player
+          <PlayerChoice
+            choice={[
               currentQuestion.choice1,
               currentQuestion.choice2,
               currentQuestion.choice3,
               currentQuestion.choice4,
-            ]} handleAnswerClick={handleAnswerClick} />
-          ) : (
-            <div className="text-4xl">Oops! something wrong!</div>
-          )
-        }
+            ]}
+            handleAnswerClick={handleAnswerClick}
+          />
+        ) : (
+          <div className="text-4xl">Oops! something wrong!</div>
+        )}
       </div>
     </div>
   );
